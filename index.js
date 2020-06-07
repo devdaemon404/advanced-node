eaac350fd9c6d23c49b1669db95d49f9bca18cb9
Bharath Karanth <bharathkaranth07@gmail.com>
2020-06-07 21:40:13 +0530

    added models,middlewares and services to our app

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const { cookieKey } = require('./config/keys');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+require('./services/passport');
+require('./services/cache');
+
+connectDB();
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+require('./routes/blogRoutes')(app);
+
+if (['production','ci'].includes(process.env.NODE_ENV)) {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on port`, PORT);
+});
